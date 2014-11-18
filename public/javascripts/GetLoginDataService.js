b8b15061e75554c4c1f188e9b5f9ac9f700415f2
lukas.schmalzbauer <schmalzbauer.lukas94@gmail.com>
2014-11-18 13:36:14 +0100

    HTTP GET for Login Form created

---
+++ public/javascripts/GetLoginDataService.js
@@ -0,0 +1,8 @@
+angular.module('TabsApp')
+    .factory('GetLoginDataService', ['$http', function ($http) {
+        return {
+            getLogin:function(){
+                return $http.get("/loginData");
+            }
+        };
+    }]);
--- public/javascripts/LoginForm.js
+++ public/javascripts/LoginForm.js
@@ -1,9 +1,15 @@
 angular.module('TabsApp')
-    .controller('LoginFormCtrl', ['LoginService',function (LoginService) {
+    .controller('LoginFormCtrl', ['LoginService','GetLoginDataService',function (LoginService,GetLoginDataService) {
         var self = this;
 
         self.sendFormData = function(){
             LoginService.login(self.user);
+        }
+
+        self.receivedLoginData = {};
+
+        self.getFormData = function(){
+            self.receivedLoginData = GetLoginDataService.getLogin();
         }
     }]);
 
