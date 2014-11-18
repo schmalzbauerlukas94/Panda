f299c26a5523184c76518b7c15db912ff6ce2c39
marku_000 <marku_000@Client01>
2014-11-18 18:47:58 +0100

    Hallo

---
+++ app/controllers/UserLogin.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+
+
+import play.*;
+import play.mvc.*;
+
+public class UserLogin extends Controller{
+
+	  public static Result save() {
+		  
+	        return ok("ABC");
+	    }
+	
+}
